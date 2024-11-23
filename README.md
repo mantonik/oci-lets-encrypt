7d71fc3dae74ea161d30bda840cb6cfb2481cd32
mantonik <mantonik@gmail.com>
2024-11-23 09:24:10 -0500

    update files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # oci-lets-encrypt
 Scripts to create a lets encrypt certificate and update load balancer
+
+unzip this package to bin folder 
+it will create a folder oci-lets-encrytp 
+with subset of the folders where you will configure envirement 
+
+start with oci-init.sh script - script will install required packaged for oci, setup a rsa key and install certboot.
+
+oci-init.sh 
+
+User generate key to create API key on your account - admin account which have access to manage load balancer
+
+
+
+
---
+++ oci-lets-encrypt/bin/oci-init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Script will initialize account 
+echo "Script will install required packages for certbot and setup certificate for"
+
+# Install oci tools as root
+echo "----" 
+echo "Install OCI tools"
+mkdir /root/install
+cd /root/install
+wget https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
+mv install.sh oci_install.sh
+chmod 700 oci_install.sh
+./oci_install.sh --accept-all-defaults
+rm -f ./oci_install.sh
+
+bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh) --accept-all-defaults"
+
+
+#Ceate rsa key for oci
+mkdir ~/.oci
+cd ~/.oci
+openssl genrsa -out ~/.oci/oci_api_key.pem -aes128 2048   
+no passphrase 
+openssl genrsa -out ~/.oci/oci_api_key.pem 2048   
+chmod go-rwx ~/.oci/oci_api_key.pem  
+openssl rsa -pubout -in ~/.oci/oci_api_key.pem -out ~/.oci/oci_api_key_public.pem 
+
+
+echo "-----"
+echo "Install Certboot"
+python -m ensurepip --upgrade
+get-pip.py
+pip3 install certbot
+
+#Create a root rsa key 
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa_rsync  -q -N ""
+cp ~/.ssh/id_rsa_rsync.pub /share/root_app1_id_rsa_rsync.pub
+chown 600 ~/.ssh/*
